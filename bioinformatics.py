76ecea033e7273ff8f338b0bcbbf8b2501eebb0e
Thomas Wilkins <thomaswilkins@Thomass-MacBook-Pro.local>
2015-01-30 11:46:54 -0700

    Upadated bioinformatics

--- bioinformatics.py
+++ bioinformatics.py
@@ -41,4 +41,53 @@ def frequent_words(text, k):
 			infrequent_words.append(pattern)
 	return frequent_words
 
+def reverse_complement(pattern):
+	'''
+	returns the reverse complement of the pattern string
+	'''
+	#translation dictionary, includes lower and upper case for more robust input acceptance
+	complements = {'a':'t',
+				'A':'T',
+				't':'a',
+				'T':'A',
+				'g':'c',
+				'G':'C',
+				'c':'g',
+				'C':'G'}
+	#construct a new string using the dictionary to translate, then revere it with the slice
+	return ''.join([complements.get(char,char) for char in pattern])[::-1]
+
+def pattern_match(pattern, genome):
+	'''
+	if the pattern is found in the genome, return the indices that it is found at.
+	if not, return an empty list
+	'''
+	indices = []
+	pattern_length = len(pattern)
+	for i in range((len(genome) - pattern_length) + 1):
+		if genome[i:i + pattern_length] == pattern:
+			indices.append(i)
+	return indices
+
+def clump_finding(genome, k, L, t):
+	'''
+	Searches substrings of length L in genome for all k-mers that occur at least t times
+	'''
+	clumps = []
+	#for all substrings of length L in genome
+	for i in range((len(genome) - L) + 1):
+		#for all patterns in each substring
+		searched_words = []
+		for j in range(L - k + 1):
+			pattern = genome[j:j + k]
+			if pattern in searched_words or pattern in clumps:
+				continue
+			occurences = count(genome[i:i + L],pattern)
+			if occurences >= t:
+				clumps.append(pattern)
+			searched_words.append(pattern)
+	return clumps	
+
+
+
 
